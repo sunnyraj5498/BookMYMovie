73f4a22b512c605b56a83ce2a5b0ce7f92d0a722
sunnyraj5498 <91817851+sunnyraj5498@users.noreply.github.com>
2021-11-30 19:41:22 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+seating=[]
+
+class Seats:
+    def __init__(self,name,gender,age,phone,price):
+        self.name=name
+        self.gender=gender
+        self.age=age
+        self.phone=phone
+        self.price=price
+
+
+booked={}
+tickets_booked=0
+current_income=0
+Total_Income=0
+
